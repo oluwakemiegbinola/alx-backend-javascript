3aa2aa3263bb2f3118097984c2f8b35b337e63e7
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2024-04-08 17:01:13 -0700

    javascript

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,6 @@
+
+
+export default function createIteratorObject(report) {
+  const employeeNames = Object.values(report.allEmployees);
+  return employeeNames.map((employeeName) => employeeName).flat();
+}
